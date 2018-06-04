514ba9dd2cb4a8003f408326d79d9d79366b40d4
Tyler Poon <tylerpoon@gmail.com>
2018-06-03 19:21:20 -0700

    Init

--- lexer.c
+++ lexer.c
@@ -13,6 +13,38 @@ lexer_t init_lexer(char *filename) {
   return lex;
 }
 
+token_t next_idkey(lexer_t *self, c) {
+  token_t token;
+  char c;
+  do {
+    c = getc(self->fp);
+    strcat(token.value, c);
+  } while (isalpha(c) || isdigit(c) || c == '_');
+  fseek(self->fp, -1, SEEK_CUR);
+  token.value[strlen(token.value)-1] = 0;
+
+  if (strcmp(token.value, "auto") == 0) {
+  }
+  else if (strcmp(token.value, "break") == 0) {
+  }
+  else if (strcmp(token.value, "case") == 0) {
+  }
+  else if (strcmp(token.value, "char") == 0) {
+  }
+  else if (strcmp(token.value, "const") == 0) {
+  }
+
+}
+
+token_t next_float(lexer_t *self, c) {
+}
+
+token_t next_num(lexer_t *self, c) {
+}
+
+token_t next_string(lexer_t *self, c) {
+}
+
 token_t next_token(lexer_t *self) {
   token_t token;
 
@@ -21,6 +53,11 @@ token_t next_token(lexer_t *self) {
   offset += 1;
   switch (c) {
     case EOF: token.type = EOFTOKEN; return token;
+    case ' ':
+    case '\t':
+    case '\n':
+    case '\r':
+              return next_token(self);
     case '[': token.type = LBRACKET; return token;
     case ']': token.type = RBRACKET; return token;
     case '(': token.type = LPAREN; return token;
@@ -35,12 +72,14 @@ token_t next_token(lexer_t *self) {
                 token.type = ELLIPSIS;
                 token.value = strcat(token.value, c1);
                 token.value = strcat(token.value, c2);
+              } else if (isdigit(c)) {
                 fseek(self->fp, -2, SEEK_CUR);
-                return token;
+                next_float(c);
               } else {
                 token.type = DOT;
-                return token;
+                fseek(self->fp, -2, SEEK_CUR);
               }
+              return token;
     case '-':
               char c1 = getc(self->fp);
               offset += 1;
@@ -84,46 +123,40 @@ token_t next_token(lexer_t *self) {
     case '*':
               char c1 = getc(self->fp);
               offset += 1;
-              token.value = strcat(token.value, c1);
               if(c1 == '=') {
+                token.value = strcat(token.value, c1);
                 token.type = MULASSIGN;
-                return token;
               } else {
-                token.value = token.value[0];
                 fseek(self->fp, -1, SEEK_CUR);
                 offset -= 1;
                 token.type = STAR;
-                return token;
               }
+              return token;
     case '~': token.type = UNARY; return token;
     case '!':
               char c1 = getc(self->fp);
               offset += 1;
-              token.value = strcat(token.value, c1);
               if(c1 == '=') {
                 token.type = NEQ;
-                return token;
+                token.value = strcat(token.value, c1);
               } else {
-                token.value = token.value[0];
                 fseek(self->fp, -1, SEEK_CUR);
                 offset -= 1;
                 token.type = NOT;
-                return token;
               }
+              return token;
     case '/':
               char c1 = getc(self->fp);
               offset += 1;
-              token.value = strcat(token.value, c1);
               if(c1 == '=') {
                 token.type = DIVASSIGN;
-                return token;
+                token.value = strcat(token.value, c1);
               } else {
-                token.value = token.value[0];
                 fseek(self->fp, -1, SEEK_CUR);
                 offset -= 1;
                 token.type = DIV;
-                return token;
               }
+              return token;
     case '%':
               char c1 = getc(self->fp);
               offset += 1;
@@ -159,6 +192,19 @@ token_t next_token(lexer_t *self) {
                 case ':': token.type = LBRACKETALT; return token;
                 case '%': token.type = LBRACEALT; return token;
                 case '<':
+                          char c2 = getc(self->fp);
+                          offset += 1;
+                          if (c2 == '=') {
+                            token.type = SHLASSIGN;
+                            offset += 1;
+                            token.value = strcat(token.value, c2);
+                          } else {
+                            fseek(self->fp, -1, SEEK_CUR);
+                            token.type = SHL;
+                          }
+
+                          return token;
+
                 default: token.value = token.value[0];
                          fseek(self->fp, -1, SEEK_CUR);
                          offset -= 1;
@@ -166,14 +212,98 @@ token_t next_token(lexer_t *self) {
                          return token;
               }
     case '>':
+              char c1 = getc(self->fp);
+              offset += 1;
+              token.value = strcat(token.value, c1);
+              switch(c1) {
+                case '=': token.type = GREATERTHANEQUAL; return token;
+                case '>':
+                          char c2 = getc(self->fp);
+                          offset += 1;
+                          if (c2 == '=') {
+                            token.type = SHRASSIGN;
+                            offset += 1;
+                            token.value = strcat(token.value, c2);
+                          } else {
+                            fseek(self->fp, -1, SEEK_CUR);
+                            token.type = SHR;
+                          }
+                          return token;
 
+                default: token.value = token.value[0];
+                         fseek(self->fp, -1, SEEK_CUR);
+                         offset -= 1;
+                         token.type = GREATERTHAN;
+                         return token;
+              }
     case '=':
+              char c1 = getc(self->fp);
+              offset += 1;
+              if(c1 == '=') {
+                token.type = EQ;
+                token.value = strcat(token.value, c1);
+              } else {
+                fseek(self->fp, -1, SEEK_CUR);
+                offset -= 1;
+                token.type = ASSIGN;
+              }
+              return token;
     case '^':
+              char c1 = getc(self->fp);
+              offset += 1;
+              if(c1 == '=') {
+                token.type = XORASSIGN;
+                token.value = strcat(token.value, c1);
+              } else {
+                fseek(self->fp, -1, SEEK_CUR);
+                offset -= 1;
+                token.type = BITXOR;
+              }
+              return token;
     case '|':
-    case '?':
+              char c1 = getc(self->fp);
+              offset += 1;
+              token.value = strcat(token.value, c1);
+              switch(c1) {
+                case '|': token.type = LOGOR; return token;
+                case '=': token.type = ORASSIGN; return token;
+                default: token.value = token.value[0];
+                         fseek(self->fp, -1, SEEK_CUR);
+                         offset -= 1;
+                         token.type = BITOR;
+                         return token;
+              }
+    case '?': token.type = QMARK; return token;
     case ':':
-    case ';':
-    case ',':
+              char c1 = getc(self->fp);
+              offset += 1;
+              if(c1 == '>') {
+                token.type = RBRACKETALT;
+                token.value = strcat(token.value, c1);
+              } else {
+                fseek(self->fp, -1, SEEK_CUR);
+                offset -= 1;
+                token.type = COLON;
+              }
+              return token;
+    case ';': token.type = SEMICOLON; return token;
+    case ',': token.type = COMMA; return token;
     case '#':
+              char c1 = getc(self->fp);
+              offset += 1;
+              if(c1 == '#') {
+                token.type = DOUBLEHASH;
+                token.value = strcat(token.value, c1);
+              } else {
+                fseek(self->fp, -1, SEEK_CUR);
+                offset -= 1;
+                token.type = HASH;
+              }
+    case '"':
+    case '\'':
+              return next_string(c);
+    default:
+              if (isalpha(c)) return next_idkey(c);
+              if (isdigit(c)) return next_num(c);
   }
 }
